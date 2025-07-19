5936ec59b57854641a3c9d15ab91c5cac7364125
InyerM <inyermedina10@gmail.com>
2025-07-19 10:38:41 -0500

    fix: update lint stage script

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const config = {
   "**/*.{ts?(x),mts}": () => "tsc -p tsconfig.json --noEmit",
-  "**/{src,test}/**/*.{ts,tsx}": ["eslint", "prettier --write"],
+  "**/{src,test}/**/*.{ts,tsx}": ["eslint"],
 };
 
 export default config;
